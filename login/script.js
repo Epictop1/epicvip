3cec86a8a12380200ccf9f842c287a6c64a6d39f
yousefehab11 <127133283+Epictop1@users.noreply.github.com>
2024-10-15 18:34:00 +0300

    1

--- login/script.js
+++ login/script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('loginForm');
 const errorMessage = document.getElementById('error-message');
+const errorMessage2 = document.getElementById('error-message2');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault(); // منع إعادة تحميل الصفحة
@@ -10,7 +11,20 @@ form.addEventListener('submit', function(e) {
     // تحقق من اسم المستخدم وكلمة المرور
     if (username === 'admin' && password === '1234') {
         window.location.href = '#'; // الانتقال إلى وضع التطوير
+        errorMessage2.textContent = 'Login Successfully'; // عرض رسالة النجاح
+        errorMessage2.style.display = 'block'; // تأكيد عرض الرسالة
+
+        // إخفاء الرسالة بعد 3 ثواني
+        setTimeout(function() {
+            errorMessage2.style.display = 'none';
+        }, 2000);
     } else {
-        errorMessage.textContent = 'اسم المستخدم أو كلمة المرور غير صحيحة.'; // عرض رسالة الخطأ
+        errorMessage.textContent = 'Incorrect username or password.'; // عرض رسالة الخطأ
+        errorMessage.style.display = 'block'; // تأكيد عرض الرسالة
+
+        // إخفاء الرسالة بعد 3 ثواني
+        setTimeout(function() {
+            errorMessage.style.display = 'none';
+        }, 2000);
     }
 });
